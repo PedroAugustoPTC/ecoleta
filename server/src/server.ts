472ef84ad3b61866e53596b91a86e9754adf93cb
unknown <pams693@gmail.com>
2020-09-22 12:24:48 -0300

    First Commit

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads'))); // carregar as imagens estaticas da pagina
 // ROTA: ENDEREÇO COMPLETO DA REQUISIÇÃO
 // RECURSO: QUAL ENTIDADE ESTAMOS ACESSANDO DO SISTEMA
 
